3bb856f92f72c04008cf5d0b75f9942fe54b0080
guanth <guantihua@163.com>
2019-01-28 22:08:32 +0800

    Initial commit

---
+++ src/main/java/com/itmuch/cloud/microservicediscoveryeurekaha/MicroserviceDiscoveryEurekaHaApplication.java
@@ -0,0 +1,16 @@
+package com.itmuch.cloud.microservicediscoveryeurekaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MicroserviceDiscoveryEurekaHaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDiscoveryEurekaHaApplication.class, args);
+    }
+
+}
+
